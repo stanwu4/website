c0a155e3ec3923ba293d8d5e624631f36a87a9f6
Stanley Wu <stanleywu0421@gmail.com>
2025-03-16 16:02:32 -0400

    re-added website files

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className = "header">
+        Stanley Wu
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className = "container">
+        Welcome to My Personal Website
+        <p>
+
+        </p>
+      </div>
+      <div className = "container">
+        Hi, I’m Stanley! <br />
+        I’m a Business and Computer Science student at the University of Michigan, passionate about the intersection of technology and finance.
+        Over the past summers, I’ve gained experience at JP Morgan working in Wealth Management and Investment Banking 
+        Credit Risk in their internship program. <br />
+        Outside of work, I love surfing, playing basketball, football, and poker. I’m a die-hard fan of the Golden State Warriors, 
+        San Francisco 49ers, and Real Madrid. I also enjoy playing competitive fps games such as Counter Strike <br />
+        I’m always eager to learn and explore how technology can shape the future of finance. Feel free to connect with me!
+        <p>
+
+        </p>
+      </div>
+      <div className = "link">
+        Contact Me: 
+        <a href = "mailto:stanleywu0421@gmail.com">Email</a>
+        <a href = "https://www.linkedin.com/in/stanleywu1/">LinkedIn</a>
+        <a href = "https://github.com/stanwu4">Github</a>
+        <a href = "https://open.spotify.com/user/162cxgi3mlnn3wpju5g505vsa?si=aab59f06435b4457">Spotify</a>
+      </div>
     </div>
   );
 }
